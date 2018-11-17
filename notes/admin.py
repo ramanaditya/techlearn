718a293ef4c0fbd41961fea5fffe75c331b27b28
ramanaditya <adityaraman96@gmail.com>
2018-11-17 20:38:48 +0530

    new

--- notes/admin.py
+++ notes/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from notes.models import notes,UserProfileInfo,comments,Feedback
+from notes.models import notes,UserProfileInfo,Comment,Feedback
 
 # Register your models here.
 admin.site.register(notes)
 admin.site.register(UserProfileInfo)
-admin.site.register(comments)
+admin.site.register(Comment)
 admin.site.register(Feedback)
