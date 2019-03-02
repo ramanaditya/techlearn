b007f0182e97f01da56f88e0eef4851eb723d940
Aditya Raman <adityaraman96@gmail.com>
2019-03-02 08:57:27 +0530

    Update settings.py

--- techlearn/settings.py
+++ techlearn/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'yq@b-l-oo=+ys^%(8^&ufh=3bm^j%l7c6e(b!)^$inwq3()r6$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'adityaraman.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'adityaraman.pythonanywhere.com','techlearn.pythonanywhere.com','beginners.pythonanywhere.com']
 
 
 # Application definition
